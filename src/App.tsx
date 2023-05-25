04617cae76e729fac871af9a55cfc4e7a647923a
Gustavo Gomes Ferreira <gustavogomesferreira2012@outlook.com>
2023-05-25 15:33:19 -0300

    feat: prettier-eslint

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native/types";
+
+const App = () => {
+    return(
+        <SafeAreaView>
+            <Text>Hello Word</Text>
+        </SafeAreaView>
+    );
+}
+
+export default App;
