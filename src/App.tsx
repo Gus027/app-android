6e5e5df4392033534f9eed348cdeed205f5edea4
Gustavo Gomes Ferreira <gustavogomesferreira2012@outlook.com>
2023-05-25 15:47:52 -0300

    feat: styledCompanents

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { SafeAreaView, Text } from "react-native/types";
+import { SafeAreaView, Text } from 'react-native/types';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+    color: blue;
+`;
 
 const App = () => {
-    return(
-        <SafeAreaView>
-            <Text>Hello Word</Text>
-        </SafeAreaView>
-    );
-}
+  return (
+    <SafeAreaView>
+      <TextNew>OLA MUNDO</TextNew>  
+      <Text>Hello Word</Text>
+    </SafeAreaView>
+  );
+};
 
 export default App;
